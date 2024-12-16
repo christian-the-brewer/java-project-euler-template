0d8746b9699f2a3a261b0b74bc135553f5dc545b
christian-the-brewer <brewer.christiang@gmail.com>
2024-12-15 21:08:23 -0500

    test fetching page was successful

---
+++ eulerJavaTemplate/src/EulerJavaTemplate.java
@@ -0,0 +1,14 @@
+public class EulerJavaTemplate {
+    public static void main(String[] args) {
+
+        // check that argument was added
+        if (args.length == 0) {
+            System.out.println("No problem number given.");
+            System.out.println("Usage: java -jar EulerJavaTemplate.jar <number of problem>");
+            return;
+        }
+
+        Problem problem = new Problem(Integer.parseInt(args[0]));
+        problem.fetchPage();
+    }
+}
