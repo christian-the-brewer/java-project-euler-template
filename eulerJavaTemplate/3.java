13dca6a2ca8bc900e1902285cbba65d3115f7f7b
christian-the-brewer <brewer.christiang@gmail.com>
2024-12-17 21:38:35 -0500

    adds check and confirmation before overwriting

--- eulerJavaTemplate/3.java
+++
@@ -1,9 +0,0 @@
-// Project Euler Problem #3: Largest Prime Factor
-// Author: Christian Brewer
-// Date: 2024-12-17
-// The prime factors of 13195 are 5, 7, 13 and 29.What is the largest prime factor of the number 600851475143?
-
-public class EulerJava3 {
-	public static void main(String[] args) {
-	}
-}
--- eulerJavaTemplate/src/EulerJavaTemplate.java
+++ eulerJavaTemplate/src/EulerJavaTemplate.java
@@ -1,6 +1,8 @@
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.time.LocalDate;
+import java.io.File;
+import java.util.Scanner;
 
 
 public class EulerJavaTemplate {
@@ -20,6 +22,22 @@ public class EulerJavaTemplate {
         problem.fetchPage(); //get page from projecteuler.net
         problem.parsePage(); //get title and prompt from HTML
 
+        //check if file already exists, if it does, ask for confirmation
+        File file = new File(fileName);
+        if (file.exists()) {
+            System.out.println("File already exists. Overwrite? [y/n]");
+            Scanner scanner = new Scanner(System.in);
+            if (!scanner.next().toLowerCase().equals("y")) {
+                scanner.close();
+                System.out.println("Aborting...");
+                return;
+            } else {
+                System.out.println("Overwriting...");
+            }
+            scanner.close();
+        }
+
+
         //create file with starter code
         try (PrintWriter write = new PrintWriter(fileName)) {
             write.println("// Project Euler Problem #" + problemNumber + ": " + problem.getTitle());
@@ -27,6 +45,7 @@ public class EulerJavaTemplate {
             write.println("// Date: " + LocalDate.now());
             write.println("// " + problem.getPrompt());
             write.println("\npublic class EulerJava" + problemNumber + " {\n\tpublic static void main(String[] args) {\n\t}\n}");
+            System.out.println(fileName + " created.");
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         }
